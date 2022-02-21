117d4d177701b370e4d38b8ad5a87f81ffaeaeaf
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-21 16:03:42 +0330

    Updating index.js

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-console.log('Hello World!');
+let data = [
+  2,
+  'ashkan',
+  99,
+  [
+    2,
+    'ashkan',
+    99,
+    true,
+    new Date(2014, 0, 31),
+    -6.3,
+    'as a man',
+    new Date(2012, 0, 3),
+  ],
+  { number: 1372, age: { number: 'ashansn' } },
+  true,
+  new Date(2014, 0, 31),
+  -6.3,
+  'as a man',
+  new Date(2012, 0, 3),
+];
+
+/* let data = {name: "ashkan", item: [23, new Date(2012, 0, 3)]}; */
+/* let data = ["ashkan", [23, new Date(2012, 0, 3)]]; */
+
+function whatTypeIsIt(obj) {
+  // returning the type of obj
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+}
+
+function dayIncrement(date, inc = 1) {
+  // Adding [inc] days to date
+  const timeStamp = date.getTime();
+  return new Date(timeStamp + inc * 24 * 3600 * 1000);
+}
+
+function addCharToEnd(str, index) {
+  // Adding the str.charAt(index) character to the end of the str
+  // if index not provided, one of the str characters will be use
+  let rndIndex = Math.floor(Math.random() * str.length);
+  return str + str.charAt(index ?? rndIndex).toLowerCase();
+}
+
+function changeItem(item) {
+  switch (whatTypeIsIt(item)) {
+    case 'date':
+      return dayIncrement(item);
+    case 'number':
+      return item + 1;
+    case 'string':
+      return addCharToEnd(item);
+    default:
+      return item;
+  }
+}
+
+function isISOstring(str) {
+  return (
+    str.length === 24 &&
+    str.includes('T') &&
+    str.includes('Z') &&
+    (str.match(/-/g) || []).length == 2 &&
+    (str.match(/:/g) || []).length == 2
+  );
+}
+
+function objectClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function dateNumStrManipulator(obj) {
+  // ⚠ Changing the original object passed to it ⚠
+  const type = whatTypeIsIt(obj);
+  if (type === 'array' || type === 'object') {
+    for (const key in obj) {
+      obj[key] = dateNumStrManipulator(obj[key]);
+    }
+  }
+
+  if (type !== 'number' && isISOstring(obj)) obj = new Date(obj);
+
+  return changeItem(obj);
+}
+
+const newData = dateNumStrManipulator(objectClone(data));
+
+console.log(data);
+console.log(newData);
