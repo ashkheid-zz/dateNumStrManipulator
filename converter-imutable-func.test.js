411bb0aad58032811e3c1f89333c9c0b37f7623b
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-22 16:36:11 +0330

    4 Testing added

--- converter-imutable-func.test.js
+++ converter-imutable-func.test.js
@@ -1,12 +1,29 @@
 const func = require('./converter-imutable-func');
 let data = {
   name: 'ashkan',
-  skils: {
+  age: 28,
+  skills: {
     code: ['JS', 'HTML', 'CSS'],
     music: ['kalimba', 'piano', 'clarinet'],
   },
 };
+
 test('expect ashkan to be one character longer', () => {
   const result = func(data);
   expect(data.name.length).toBeLessThan(result.name.length);
+});
+
+test('That age is expected to be one unit greater', () => {
+  const result = func(data);
+  expect(result.age).toEqual(data.age + 1);
+});
+
+test('the original date should\'ve been remained untouched', () => {
+  const result = func(data);
+  expect(result).not.toEqual(data);
+});
+
+test('result should not be undefined', () => {
+  const result = func(data);
+  expect(result).not.toBeUndefined();
 });
