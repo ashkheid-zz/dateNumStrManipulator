a3373f58082d3d8ce3db8603412f10cc2e15c6d6
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-22 19:08:05 +0330

    Refactoring transform()
    an empty object pased to the function in order to filled with new transfomed data

---
+++ converter-immutable-func-v2.js
@@ -0,0 +1,56 @@
+let data = {
+  name: 'ashkan',
+  skills: {
+    code: ['JS', 'HTML', 'CSS'],
+    music: ['kalimba', 'piano', 'clarinet'],
+  },
+};
+// let data = { name: 'ashkan', info: { age: 23, father: 'Reza', info: { age: 47, father: 'Sezar' }} };
+
+function whatTypeIsIt(obj) {
+  // returning the type of obj
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+}
+
+function changeItem(item) {
+  switch (whatTypeIsIt(item)) {
+    case 'number':
+      return item + 1;
+    case 'string':
+      return item + "-zZz";
+    default:
+      return item;
+  }
+}
+
+console.log('Before working on data 👇🏻');
+console.log(data);
+
+
+function transform(obj, newObj = {}) {
+  // Changing the object passed to it
+
+  for (const key in obj) {
+
+    const valueType = whatTypeIsIt(obj[key]);
+    // checking type of the obj[key] which might be either 'object' or 'array'
+    
+    newObj[key] =
+      valueType === 'object'
+        ? transform(obj[key])
+        : valueType === 'array'
+        ? obj[key].map((item) => changeItem(item))
+        : changeItem(obj[key]);
+  }
+
+  return newObj;
+}
+
+let newData = {}
+transform(data, newData);
+
+console.log('After working on data 👇🏻');
+console.log(data);
+
+console.log('New data 👇🏻');
+console.log(newData);
